2059e48f8104e54787fd5043804e9802f19df522
netanel <netaneal6105@gmail.com>
2022-10-26 12:37:15 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css';
-import First from './components/first';
-import Seccend from './components/seccend';
+
 
 function App() {
   return (
     <div >
-     {/* <Seccend/> */}
-     <First/>
+    
     </div>
 
 
--- src/components/seccend.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Seccend extends Component {
-  render() {
-    return (
-      <h1>this is fucking second mission Peaky Fookin Blinders!!!!!!!!!!!!!!!!</h1>
-    )
-  }
-}
