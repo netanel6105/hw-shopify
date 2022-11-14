639ac7b138f3809f76f1751c03cf206246a068df
netanel <netaneal6105@gmail.com>
2022-11-14 15:12:52 +0200

    ok

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='strip'>
-      <div>
-        
+    <div className='strip container-fluid '>
+      <div className='container'>
+      <div className='nav-left p-3 d-flex'>
+        <img src="./images/shopify-2.jpg" alt="" />
+                   
+        <ul>              
+          <li>Start</li>
+          <li>Sell</li>
+          <li>Market</li>
+          <li>Manage</li>
+        </ul>
+         
       </div>
+      </div>
+
     </div>
   )
 }
 
 export default Navbar
+
+
+
