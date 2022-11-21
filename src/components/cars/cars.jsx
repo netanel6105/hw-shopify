e5b30a6bd184207013b652466d05df5268105957
netanel <netaneal6105@gmail.com>
2022-11-21 13:18:13 +0200

    ok

--- src/components/cars/cars.jsx
+++ src/components/cars/cars.jsx
@@ -1,30 +1,49 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect } from 'react'
+import { useState } from 'react'
 
 
 const Cars = () => {
-
+    const [cars, setCars] = useState([]);
     const doApi = async () => {
-        try{
+        try {
             let url = `https://project-yarin.herokuapp.com/cars?perPage=99`
-            const {data} = await axios(url)
+            const { data } = await axios(url)
             console.log(data);
+            setCars(data);
         }
-        catch(err){
+        catch (err) {
             console.log(err.response);
         }
     }
-       
-    useEffect(()=>{
+
+    useEffect(() => {
         doApi();
-    },[])
+    }, [])
 
 
 
     return (
         <div>
-      <h1 className='disply-3 text-center' >cars</h1>
+            <h1 className='disply-3 text-center' >cars</h1>
+
+            <div style={{color:"blue"}} className='container' >
+                <div className="row">
+                    
+                {cars?.map((item, i) => (
+                    <div key={i} className='shadow p-2 mt-3 my-3 border col-12 col-md-6 col-lg-3 border'>
+                        <p > {item.model}</p>
+                        <p > {item.company}</p>
+                    </div>
+                ))}
+                </div>
+            </div>
+
+
+
+
+           
         </div>
     )
 }
